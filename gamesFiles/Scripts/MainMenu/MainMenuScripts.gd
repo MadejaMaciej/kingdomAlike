905fafed2b5eb6105572a69c40e9e9fdbf33bf31
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-03-26 19:04:55 +0100

    created a testmap with test tiles

---
+++ gamesFiles/Scripts/MainMenu/MainMenuScripts.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var level = "res://Maps/MainMenu.tscn"
+var nextLevel = "res://Maps/devRoom.tscn"
+
+func _on_StartGame_pressed():
+	get_tree().change_scene(nextLevel)
+
+
+
+func _on_QuitGame_pressed():
+	get_tree().quit()
