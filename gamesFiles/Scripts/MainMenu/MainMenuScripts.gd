ee3bc7c3ad4608dd23f8654c19f6374f3b09b64b
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-03-27 12:07:40 +0100

    Added second cave and background

--- gamesFiles/Scripts/MainMenu/MainMenuScripts.gd
+++ gamesFiles/Scripts/MainMenu/MainMenuScripts.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var level = "res://Maps/MainMenu.tscn"
-var nextLevel = "res://Maps/devRoom.tscn"
+var nextLevel = "res://Maps/level.tscn"
 
 func _on_StartGame_pressed():
 	get_tree().change_scene(nextLevel)
