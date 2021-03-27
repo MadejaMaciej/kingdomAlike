2d52011f6e8de96d1f8a60754f4b76b350ec63fb
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-03-27 11:45:36 +0100

    Ended first cave how it should be and added animations and rotating player

--- gamesFiles/Scripts/Player/player.gd
+++ gamesFiles/Scripts/Player/player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export var speed = 400
 var screen_size
+var lastMove
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -10,11 +11,19 @@ func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
+		lastMove = 'right'
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
+		lastMove = 'left'
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
 		velocity = move_and_slide(velocity)
+		if lastMove == 'left':
+			$AnimatedSprite.play("Run_left")
+		else:
+			$AnimatedSprite.play("Run_right")
 	else:
-		$AnimatedSprite.stop()
+		if lastMove == 'left':
+			$AnimatedSprite.play("Idle_left")
+		else:
+			$AnimatedSprite.play("Idle_right")
