31c657e2fa19ee34c210b32173afb493ba65dfb0
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-05-30 10:31:33 +0200

    last version done

--- gamesFiles/Scripts/MainMenu/MainMenuScripts.gd
+++ gamesFiles/Scripts/MainMenu/MainMenuScripts.gd
@@ -46,3 +46,43 @@ func load_game():
 
 func _on_LoadGame_pressed():
 	load_game()
+
+
+func _on_HowToPlay_pressed():
+	$MenuElements.hide()
+	$Slides.show()
+
+
+func _on_Next2_pressed():
+	$Slides/s1.hide()
+	$Slides/s2.show()
+
+
+func _on_Prev1_pressed():
+	$Slides/s2.hide()
+	$Slides/s1.show()
+
+
+func _on_Next3_pressed():
+	$Slides/s2.hide()
+	$Slides/s3.show()
+
+
+func _on_Prev2_pressed():
+	$Slides/s3.hide()
+	$Slides/s2.show()
+
+
+func _on_Next4_pressed():
+	$Slides/s3.hide()
+	$Slides/s4.show()
+
+
+func _on_Prev3_pressed():
+	$Slides/s4.hide()
+	$Slides/s3.show()
+
+
+func _on_MainMenuBack_pressed():
+	$Slides.hide()
+	$MenuElements.show()
